56b9d081e05395804df812ce347cb1ecbf8fc85d
nitin <nitinvardha09@gmail.com>
2022-10-06 09:27:37 -0500

    default types

---
+++ sandbox.ts
@@ -0,0 +1,19 @@
+let character = 'mario';
+let age =  30;
+let isBlackBelt = false;
+
+
+// character = 30
+character = 'luigi';
+
+// age = 'yoshi';
+age = 40;
+
+// isBlackBelt = 'yes';
+isBlackBelt = true;
+
+const circ = (diameter: number) =>{
+    return diameter * Math.PI;
+}
+
+console.log(circ(20));
