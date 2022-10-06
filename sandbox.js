13e4d29fa8706c4b7b9dff6705085685528449ba
nitin <nitinvardha09@gmail.com>
2022-10-06 09:41:31 -0500

    arrays and objects

--- sandbox.js
+++ sandbox.js
@@ -1,13 +1,21 @@
-var character = 'mario';
-var age = 30;
-var isBlackBelt = false;
-// character = 30
-character = 'luigi';
-// age = 'yoshi';
-age = 40;
-// isBlackBelt = 'yes';
-isBlackBelt = true;
-var circ = function (diameter) {
-    return diameter * Math.PI;
+//arrays
+var names = ['luigi', 'mario', 'yoshi'];
+names.push('toad');
+// names.push(3);
+// names[0] = 3;
+var numbers = [10, 20, 30, 40];
+// numbers.push(25);
+// numbers.push('shaun');
+var mixed = ['ken', 4, 'chun-li', false];
+mixed.push(4);
+mixed.push('shaun');
+mixed.push(true);
+// OBJECTS
+var ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
 };
-console.log(circ(20));
+ninja.age = 40;
+ninja.name = 'ryu';
+// ninja.age='30';
--- sandbox.ts
+++ sandbox.ts
@@ -1,19 +1,28 @@
-let character = 'mario';
-let age =  30;
-let isBlackBelt = false;
+//arrays
+let names = ['luigi','mario','yoshi'];
+
+names.push('toad');
+// names.push(3);
+// names[0] = 3;
+
+let numbers = [10,20,30,40];
+// numbers.push(25);
+// numbers.push('shaun');
+
+let mixed = ['ken',4,'chun-li',false];
+mixed.push(4);
+mixed.push('shaun');
+mixed.push(true);
 
 
-// character = 30
-character = 'luigi';
+// OBJECTS
 
-// age = 'yoshi';
-age = 40;
-
-// isBlackBelt = 'yes';
-isBlackBelt = true;
-
-const circ = (diameter: number) =>{
-    return diameter * Math.PI;
+let ninja = {
+    name:'mario',
+    belt:'black',
+    age:30,
 }
 
-console.log(circ(20));
+ninja.age = 40;
+ninja.name ='ryu';
+// ninja.age='30';
