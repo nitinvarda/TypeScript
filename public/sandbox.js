67b2aae072011e6dbdaa969e404baa5f3652dd42
nitin <nitinvardha09@gmail.com>
2022-10-06 13:01:06 -0500

    functions setup

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log("hello");
+let greet;
+greet = () => {
+    console.log('hello world');
+};
+const add = (a, b, c = 10) => {
+    console.log(a + b);
+};
+add(5, 10, 'hello');
+const minus = (a, b) => {
+    return a - b;
+};
+let result = minus(10, 7);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,17 @@
-console.log("hello");
+let greet:Function;
+
+greet= () =>{
+    console.log('hello world');
+}
+
+const add =(a:number,b:number,c:number|string =10)=>{
+    console.log(a+b);
+} 
+add(5,10,'hello');
+
+
+const minus =(a:number,b:number):number =>{
+    return a-b;
+}
+
+let result = minus(10,7);
